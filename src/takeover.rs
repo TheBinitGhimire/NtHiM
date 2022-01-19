ddf8fefa79d2d0afab2b9c6575ae5d086e2ee901
Binit Ghimire <thebinitghimire@gmail.com>
2022-01-19 21:42:47 +0545

    No change on this!

--- src/takeover.rs
+++ src/takeover.rs
@@ -13,10 +13,10 @@ pub async fn _takeover(hosts: Vec<String>, threads: usize) -> std::io::Result<()
         .unwrap();
     let args = &_parse_args();
     let _timeout: u64 = args
-		.value_of("timeout")
+        .value_of("timeout")
         .unwrap_or("5")
-		.parse::<u64>()
-		.unwrap_or(5);
+        .parse::<u64>()
+        .unwrap_or(5);
 
     let fetches = iter(hosts.into_iter().map(|url| async move {
         match client
