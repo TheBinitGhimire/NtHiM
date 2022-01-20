68901928339c81b766b82fe2f75119b8564b5523
Binit Ghimire <thebinitghimire@gmail.com>
2022-01-21 02:03:32 +0545

    No changes!

--- src/main.rs
+++ src/main.rs
@@ -14,38 +14,38 @@ use std::fs::remove_file;
 use std::{path::Path, process::exit, string::String};
 
 fn main() -> std::io::Result<()> {
-	let app_dirs = AppDirs::new(Some("NtHiM"), true).unwrap();
-	let cache_file_path = app_dirs.cache_dir.join("signatures.json");
+    let app_dirs = AppDirs::new(Some("NtHiM"), true).unwrap();
+    let cache_file_path = app_dirs.cache_dir.join("signatures.json");
 
-	let args = _parse_args();
-	if !cache_file_path.exists() || args.is_present("update") {
-		println!("Updating signature cache, do wait till it gets cached!");
-		let signatures = _get_signatures_from_repo().unwrap();
-		_cache_signatures(signatures);
-	}
-	if args.is_present("output") {
-		let fileName = args.value_of("output").unwrap();
-		if Path::new(&fileName).exists() {
-			remove_file(fileName).unwrap();
-		}
-	}
-	let _threads: usize = args
-		.value_of("threads")
-		.unwrap_or("10")
-		.parse::<usize>()
-		.unwrap_or(10);
-	if args.is_present("file") && args.is_present("target") {
-		println!("Please provide either a single hostname or a file containing list of hostnames rather than both!");
-		exit(1);
-	} else if args.is_present("file") {
-		let hostnames = args.value_of("file").unwrap_or("hostnames.txt");
-		_fileRead(hostnames.to_string(), _threads);
-	} else if args.is_present("target") {
-		let _target = args.value_of("target").unwrap();
-		let mut hosts = Vec::<String>::new();
-		hosts.push(_target.to_string());
-		_takeover(hosts, _threads);
-	}
+    let args = _parse_args();
+    if !cache_file_path.exists() || args.is_present("update") {
+        println!("Updating signature cache, do wait till it gets cached!");
+        let signatures = _get_signatures_from_repo().unwrap();
+        _cache_signatures(signatures);
+    }
+    if args.is_present("output") {
+        let fileName = args.value_of("output").unwrap();
+        if Path::new(&fileName).exists() {
+            remove_file(fileName).unwrap();
+        }
+    }
+    let _threads: usize = args
+        .value_of("threads")
+        .unwrap_or("10")
+        .parse::<usize>()
+        .unwrap_or(10);
+    if args.is_present("file") && args.is_present("target") {
+        println!("Please provide either a single hostname or a file containing list of hostnames rather than both!");
+        exit(1);
+    } else if args.is_present("file") {
+        let hostnames = args.value_of("file").unwrap_or("hostnames.txt");
+        _fileRead(hostnames.to_string(), _threads);
+    } else if args.is_present("target") {
+        let _target = args.value_of("target").unwrap();
+        let mut hosts = Vec::<String>::new();
+        hosts.push(_target.to_string());
+        _takeover(hosts, _threads);
+    }
 
-	Ok(())
+    Ok(())
 }
